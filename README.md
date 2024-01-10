8f84753487f0489a952e8dfb401a7ae6fc348f2e
eseverett <eseveret@asu.edu>
2024-01-10 09:28:35 -0700

    changing theme in conf.py

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 This repository is the documentation for CXLS ionizing radiation and laser interlock system. This is under construction currently and will be updated regularly.
 
-This is also being deployed at [GitLab](https://gitlab.com/eseverett/cxls_interlock_system_documentation).
-
 [Click here to access the documentation.](https://eseverett.github.io/CXLS_Interlocks_Docs/)
--- source/conf.py
+++ source/conf.py
@@ -30,8 +30,7 @@ release = '2024'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.duration'] ### Added extension
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,15 +40,24 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+pygments_style = 'sphinx' ### Added style
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' ### Changed theme to alabaster
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = ['custom.css'] ### Added custom css file
+
+# Pygments (syntax highlighting) style to use
+pygments_style = 'friendly' ### Added style
+
+# This is the correct place for html_add_permalinks configuration
+html_permalinks = False  ### Disable ¶ symbols next to headings
