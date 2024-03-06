8ea1b2336eb275edad25408caead7e61ad128343
eseverett <eseveret@asu.edu>
2024-03-06 11:18:14 -0700

    removed custom variable, testing settings in github

--- docs/source/extensions/pdf_button.py
+++ docs/source/extensions/pdf_button.py
@@ -20,7 +20,7 @@ class PDFButtonDirective(SphinxDirective):
         # Check if explicitly marked as a local build
         
         on_gitlab = os.environ.get('GITLAB_CI') == 'true'
-        on_github = os.environ.get('ON_GITHUB') == 'true' # custom environment variable
+        on_github = os.environ.get('GITHUB_ACTIONS') == 'true' # custom environment variable
 
         if on_gitlab:
             pdf_path = f'/_static/{pdf_filename}'
